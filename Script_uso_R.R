52d5d85aa50167975dbe17486962a319363eed92
jcayun <jcayun@ug.uchile.cl>
2021-10-11 22:29:09 -0300

    actualizacion

--- Script_uso_R.R
+++ Script_uso_R.R
@@ -3,3 +3,11 @@
 #Atajos
 
 echo=FALSE, comment=NA, results='hide',message=FALSE, warning = FALSE
+
+token
+
+
+R:GITHUB_PAT
+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
+
+credentials::set_github_pat("YourPAT")
